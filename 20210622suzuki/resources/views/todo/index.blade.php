6e92b6efb256c7035d3c76e30a0f7f59a9257372
Jun <whitte_rabbit@hotmail.com>
2021-06-23 14:05:51 +0900

    todo-app

--- 20210622suzuki/resources/views/todo/index.blade.php
+++ 20210622suzuki/resources/views/todo/index.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="{{ secure_asset('css/reset.css') }}">
-  <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <title>Todo</title>
 </head>
 
