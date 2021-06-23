97ad22d96c331f365601294556852c1a3fe40b77
Jun <whitte_rabbit@hotmail.com>
2021-06-23 14:02:30 +0900

    todo-app

--- 20210622suzuki/resources/views/todo/index.blade.php
+++ 20210622suzuki/resources/views/todo/index.blade.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('css/reset.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
   <title>Todo</title>
 </head>
 
--- 20210622suzuki/routes/web.php
+++ 20210622suzuki/routes/web.php
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodoController::class, 'index']);
+
 
 Route::get('/todo', [TodoController::class, 'index']);
 Route::post('/todo', [TodoController::class, 'routing']);
